0597d4a91ffa5cf5110ce0fcdcf63139e6dd9a0e
HOANG TUNG 2 <tunganh99@gmail.com>
2023-05-16 10:13:04 +0700

    second commit

--- src/test/java/com/tungphongdo/DemoSpringJenkinsApplicationTests.java
+++ src/test/java/com/tungphongdo/DemoSpringJenkinsApplicationTests.java
@@ -2,11 +2,14 @@ package com.tungphongdo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class DemoSpringJenkinsApplicationTests {
 
 	private static Logger logger = LoggerFactory.getLogger(DemoSpringJenkinsApplication.class);
@@ -14,6 +17,7 @@ class DemoSpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Application start executing .....");
+		logger.info("Application start executing the second time.....");
 		Assertions.assertTrue(true);
 	}
 
